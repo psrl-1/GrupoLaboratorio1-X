906a42e4fe1ce2ff69f3f670f9e2f0015718ed72
KMancia07 <00039426@uca.edu.sv>
2026-09-09 14:41:19 -0600

    Funcion ConsultarCalorias

--- calorias_diarias.cc
+++ calorias_diarias.cc
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <windows.h>
 
-
-int calorias = 2000;
+void ConsultarCalorias (float calorias);
+float calorias = 2000;
 
 int main ()
 {
   SetConsoleOutputCP(CP_UTF8);
     
   return 0;
+}
+
+void ConsultarCalorias (float calorias)
+
+{
+  std:: cout <<"Tus calorias son: " <<calorias << "cal" << std:: endl;
+
 }
