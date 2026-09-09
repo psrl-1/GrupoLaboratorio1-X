0980c99c418f1c4d5c59471360555189d4659a04
Valeria Abigail Vásquez Martínez <00023826@uca.edu.sv>
2026-09-09 15:08:19 -0600

    feat: Funcion quemar calorias

--- calorias_diarias.cc
+++ calorias_diarias.cc
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <windows.h>
 
-void consultar_calorias (float calorias);
+void consultar_calorias(float calorias);
 void AgregarCalorias(float &calorias, int calorias_nuevas);
 
 float calorias = 2000;
 
-int main ()
+int main()
 {
   SetConsoleOutputCP(CP_UTF8);
-    
+
   return 0;
 }
 
 void QuemarCalorias(int *calorias, int calorias_quemadas)
 {
-    *calorias -= calorias_quemadas;
-void consultar_calorias (float calorias)
+  *calorias -= calorias_quemadas;
+}
+void consultar_calorias(float calorias)
 
 {
-  std:: cout <<"Tus calorias son: " <<calorias << "cal" << std:: endl;
-
+  std::cout << "Tus calorias son: " << calorias << "cal" << std::endl;
+}
 void AgregarCalorias(float &calorias, int calorias_nuevas)
 {
   calorias += calorias_nuevas;
