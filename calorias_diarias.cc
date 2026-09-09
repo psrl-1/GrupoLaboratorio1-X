4fedf7a69bbd0f77d9274e8371acf173450e332e
Paola Rodriguez <00041826@uca.edu.sv>
2026-09-09 15:25:56 -0600

    Main calorias

--- calorias_diarias.cc
+++ calorias_diarias.cc
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <windows.h>
 
-void consultar_calorias(float calorias);
+void ConsultarCalorias(float calorias);
 void AgregarCalorias(float &calorias, int calorias_nuevas);
+void QuemarCalorias(float *calorias, int calorias_quemadas);
 
 float calorias = 2000;
 
 int main()
 {
+
   SetConsoleOutputCP(CP_UTF8);
+
+  int calorias_quemadas;
+  ConsultarCalorias(calorias);
+
+  // Agregar 100 calorias
+  AgregarCalorias(calorias, 100);
+  ConsultarCalorias(calorias);
 
   return 0;
 }
 
-void QuemarCalorias(int *calorias, int calorias_quemadas)
+void QuemarCalorias(float *calorias, int calorias_quemadas)
 {
   *calorias -= calorias_quemadas;
 }
-void consultar_calorias(float calorias)
-
+void ConsultarCalorias(float calorias)
 {
   std::cout << "Tus calorias son: " << calorias << "cal" << std::endl;
 }
